fdbdec3c7934ad705e7e90edb491a17165d89284
jdagogo <your-email@example.com>
2025-10-13 18:24:03 -0700

    Update commit badge to e64b157

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -353,7 +353,7 @@ export default function Home() {
                       v4.5-development
                     </code>
                     <span className="text-blue-700 text-sm">
-                      | Commit: <span className="font-semibold">9aeedeb</span>
+                      | Commit: <span className="font-semibold">e64b157</span>
                     </span>
                   </div>
                 </div>
